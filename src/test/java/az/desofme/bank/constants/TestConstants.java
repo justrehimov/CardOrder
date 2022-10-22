fd9cfa531044dd3c8c343c9935de60c7bacbdf00
RahimovVM <vusal.rahimov@kapitalbank.az>
2022-10-22 12:59:53 +0400

    Unit tests was added

--- src/test/java/az/desofme/bank/constants/TestConstants.java
+++ src/test/java/az/desofme/bank/constants/TestConstants.java
@@ -4,6 +4,11 @@ public final class TestConstants {
 
     public static final String ROLE_NAME = "CUSTOMER";
     public static final String PIN = "779ULMV";
+    public static final String EMAIL = "vusalr06@gmail.com";
+    public static final String NAME = "Vusal";
+    public static final String SURNAME = "Surname";
+    public static final String PASSWORD = "12345678";
+    public static final String AUTH_TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI3NzlVTE1WIiwiaWF0IjoxNjY1NjY4MjU4LCJpc3MiOiJERVNPRk1FX0JBTksiLCJleHAiOjE5NjU2NjgyNTh9.-vDy80mlEP3mppIH280ZezWSY2Rw26TPFtE-DnpcSl-2938AtZ9N0We1jmFzcgG2U_4WiuJPu1yYwODULvZ3Cg";
 
     private TestConstants() {
 
--- src/test/java/az/desofme/bank/service/AuthServiceTest.java
+++ src/test/java/az/desofme/bank/service/AuthServiceTest.java
@@ -2,16 +2,22 @@ package az.desofme.bank.service;
 
 import az.desofme.bank.dto.request.CustomerRequest;
 import az.desofme.bank.entity.Customer;
+import az.desofme.bank.exceptions.BankException;
 import az.desofme.bank.repository.CustomerRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
+import java.util.Optional;
+
+import static az.desofme.bank.constants.TestConstants.EMAIL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static az.desofme.bank.constants.TestConstants.PIN;
@@ -39,6 +45,7 @@ public class AuthServiceTest {
         //given
         var customer = new Customer();
         customer.setPin(PIN);
+        customer.setEmail(EMAIL);
 
         //when
         when(modelMapper.map(customerRequest, Customer.class)).thenReturn(customer);
@@ -53,4 +60,22 @@ public class AuthServiceTest {
         verify(modelMapper).map(customerRequest, Customer.class);
         verify(customerRepository).save(customer);
     }
+
+    @Test
+    void whenCustomerPinDuplicate_shouldReturnException() {
+
+        var customer = new Customer();
+        customer.setPin(PIN);
+
+        when(customerRepository.findByPin(PIN)).thenReturn(Optional.of(customer));
+
+        var actual = customerRepository.findByPin(PIN);
+
+        Executable executable = () -> {
+            if(actual.isPresent())
+                throw new BankException();
+        };
+        assertThrows(BankException.class, executable);
+    }
+
 }
